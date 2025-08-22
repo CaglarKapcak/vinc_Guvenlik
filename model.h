219efd8053c8bc9e266dcbac6c15f423803d162f
Çağlar <74879472+CaglarKapcak@users.noreply.github.com>
2025-08-23 00:08:27 +0300

    Create model.h

---
+++ model.h
@@ -0,0 +1,5 @@
+// TensorFlow Lite modeli - model_train.py tarafından oluşturulur
+const unsigned char g_model[] = {
+  // Buraya model_train.py tarafından oluşturulan hex değerleri gelecek
+};
+const int g_model_len = 1234;  // Gerçek model boyutu
